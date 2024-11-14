f9a4d9993f2a117423bc3518f665b6ef3218fb37
JawedBS <145364921+JawedBS@users.noreply.github.com>
2024-11-14 01:04:07 +0100

    modification étoiles

--- kasa-fr/src/Components/StarRating.js
+++ kasa-fr/src/Components/StarRating.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as faSolidStar } from '@fortawesome/free-solid-svg-icons';
-import { faStar as faRegularStar } from '@fortawesome/free-regular-svg-icons';
 import '../Styles/StarRating.css'; 
 
 const StarRating = ({ rating, maxStars = 5 }) => {
   return (
     <div className="star-rating">
-      {Array.from({ length: maxStars }, (_, index) => (
+      {Array.from({ length: maxStars }, (_, index) => {
+         const starClass = index < rating ? 'star-red' : 'star-gray';
+         return(
         <FontAwesomeIcon 
           key={index} 
-          icon={index < rating ? faSolidStar : faRegularStar}  
-          className={`star-icon ${index >= rating ? 'regular-star' : ''}`}
+          icon={faSolidStar}
+          className={`star-icon ${starClass}`}
         />
-      ))}
+         );
+})}
     </div>
   );
 };
